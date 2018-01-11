99c94535a969130ec654c21a95564de1ef872ca0
Darko Gregurek <darko.gregurek@gmail.com>
2018-01-11 14:55:33 +0100

    Java code for Portugal Score

--- app/src/main/java/com/example/android/scorekeeperapp/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeperapp/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.android.scorekeeperapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int portugalGoal = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+   public void addGoalPortugal (View V){
+        portugalGoal = portugalGoal +1;
+        displayPortugalScore(portugalGoal);
+
+
+   }
+
+
+
+
+
+
+    /**
+     * Display score for Portugal.
+     */
+    public void displayPortugalScore(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.portugal_score);
+        scoreView.setText(String.valueOf(score));
     }
 }
